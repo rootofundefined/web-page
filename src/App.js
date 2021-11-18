39a17db94edc5b702876a5de0b57ad859ec5d7ee
TheEvilRoot <robot-creatix@ya.ru>
2021-11-18 20:46:14 +0300

    cookie to local storage

--- src/App.js
+++ src/App.js
@@ -1,39 +1,5 @@
 import './App.css';
 import { useReducer } from 'react';
-
-function getCookie(name) {
-    let matches = document.cookie.match(new RegExp(
-      "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
-    ));
-    return matches ? decodeURIComponent(matches[1]) : undefined;
-}
-
-function setCookie(name, value, options = {}) {
-
-  options = {
-    path: '/',
-    // при необходимости добавьте другие значения по умолчанию
-    ...options
-  };
-
-  if (options.expires instanceof Date) {
-    options.expires = options.expires.toUTCString();
-  }
-
-  let updatedCookie = encodeURIComponent(name) + "=" + encodeURIComponent(value);
-
-  for (let optionKey in options) {
-    updatedCookie += "; " + optionKey;
-    let optionValue = options[optionKey];
-    if (optionValue !== true) {
-      updatedCookie += "=" + optionValue;
-    }
-  }
-
-  document.cookie = updatedCookie;
-}
-
-
 function Modal({onClose}) {
     return <div className="modal-wrapper">
         <div className="modal-title">
@@ -48,13 +14,13 @@ function Modal({onClose}) {
 function App() {
     const [state, updateState] = useReducer((state, act) => {
         if (act.type === 'CLOSE_DIALOG') {
-            setCookie('modal-dismissed', 'true')
+            localStorage.setItem('modal-dismissed', 'true')
             return {...state, showModalDialog: false}
         }
         return state 
     }, {}, () => {
         return { 
-            showModalDialog: getCookie('modal-dismissed') === undefined
+            showModalDialog: localStorage.getItem('modal-dismissed') !== "true" 
         }
     })
 
